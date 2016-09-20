68ab7f0dc2fd91c297bace5dc6dec7421629b040
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-09-20 18:23:13 +0300

    Update ast ns API change in render

--- src/marchio/render.clj
+++ src/marchio/render.clj
@@ -1,6 +1,6 @@
 (ns marchio.render
   (:require
-    [marchio.ast :refer [parse-ast ast->hiccup]]
+    [marchio.ast :refer [parse ast->hiccup]]
     [hiccup.core :as hiccup]))
 
 (defn md->html
@@ -8,6 +8,6 @@
   Throws an Exception on syntax error."
   [text]
   (-> text
-      (parse-ast)     ;; Parse the input text to AST
+      (parse)         ;; Parse the input text to AST
       (ast->hiccup)   ;; Convert the AST to Hiccup vectors
       (hiccup/html))) ;; Convert the Hiccup to HTML
