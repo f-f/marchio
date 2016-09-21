cae9a954513b555b21bb622a38db74b57ee3bbe3
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-09-22 02:01:28 +0300

    Finish up initial emphasis parsing logic

--- src/marchio/inlines.clj
+++ src/marchio/inlines.clj
@@ -20,20 +20,20 @@
 (defn append-children [node new]
   (update node :children conj new))
 
-(defn scan-delims
+(defn parse-emphasis
   "Search for delimiter runs (asterisk, underscore) or quotes, and return a
    vector with the rest of the string and a new Delimiter entity, with info
    about the length of the run, and if they can open or close sequences."
   [string prev-char]
-  (let [chars    (seq string)
-        before   (or (if (char? prev-char)
-                       prev-char
-                       (first (seq prev-char)))
-                     Newline)
-        char     (first chars)
-        after    (or (second chars) Newline)
-        run      (take-while (partial = char) chars)
-        run-comp (drop-while (partial = char) chars)
+  (let [chars  (seq string)
+        before (or (if (char? prev-char)
+                     prev-char
+                     (first (seq prev-char)))
+                   Newline)
+        char   (first chars)
+        after  (or (second chars) Newline)
+        [run run-comp] (mapv #(% (partial = char) chars)
+                             [take-while drop-while])
         [delims new-chars] (if (or (= char Singlequote)
                                    (= char Doublequote))
                              [1           (rest chars)]
@@ -50,18 +50,20 @@
                             (not (and before-punct?
                                       (not after-white?)
                                       (not after-punct?))))
-        [opener? closer?]
-        (condp = char
-          Underscore       [(and left-flanking
-                                 (or (not right-flanking)
-                                     before-punct?))
-                            (and right-flanking
-                                 (or (not left-flanking)
-                                     after-punct?))]
-          (or Singlequote
-              Doublequote) [(and left-flanking (not right-flanking))
-                            right-flanking]
-                           [left-flanking right-flanking])]
+        underscore-flags [(and left-flanking
+                               (or (not right-flanking)
+                                   before-punct?))
+                          (and right-flanking
+                               (or (not left-flanking)
+                                   after-punct?))]
+        quotes-flags [(and left-flanking
+                           (not right-flanking))
+                      right-flanking]
+        [opener? closer?] (condp = char
+                            Underscore  underscore-flags
+                            Singlequote quotes-flags
+                            Doublequote quotes-flags
+                            [left-flanking right-flanking])]
     (when-not (and (empty? chars) (zero? delims))
       [new-chars
        {:type char
@@ -69,9 +71,16 @@
         :opener? opener?
         :closer? closer?}])))
 
-(defn handle-delim
-  "Handle a delimiter marker for emphasis or a quote."
-  [char block])
+;; TODO: implement
+;; TODO: trasform to \u2019 if singlequote, \u201C if double
+(defn process-emph
+  "Process the children of the current node to generate emphasis inlines.
+  Stack bottom is the index of the child to wich we can go back."
+  [node stack-bottom]
+  node)
+;(let [children (subvec (:children node) stack-bottom)]
+;  (loop [current-position (min-pos (.indexOf (map :type children) Underscore)
+;                                   (.indexOf (map :type children) Asterisk)])
 
 (defn parse-next-char
   "Take a sequence of chars, return a new sequence of the rest of the chars and
@@ -79,20 +88,10 @@
   [chars prev-char]
   (let [c (first chars)]
     (condp = c
-      Asterisk   (scan-delims chars prev-char) ;; TODO: introduce handle-delims
-      Underscore (scan-delims chars prev-char)
+      Asterisk   (parse-emphasis chars prev-char)
+      Underscore (parse-emphasis chars prev-char)
       [(rest chars)
-       {:type :text :attrs {:val (str c)}}]))) ;; TODO: introduce text records
-
-;; TODO
-(defn process-emph
-  "Process the children of the current node to generate emphasis inlines.
-  Stack bottom is the index of the child to wich we can go back."
-  [node stack-bottom]
-  node)
-  ;(let [children (subvec (:children node) stack-bottom)]
-  ;  (loop [current-position (min-pos (.indexOf (map :type children) Underscore)
-  ;                                   (.indexOf (map :type children) Asterisk)])
+       {:type :text :attrs {} :children [(str c)]}]))) ;; TODO: introduce text records
 
 (defn parse-text [line block]
   (loop [chars (seq line)
@@ -104,7 +103,6 @@
       (let [[new-chars new-node] (parse-next-char chars prev)
             read-chars (- (count chars) (count new-chars))
             new-pos (+ pos read-chars)
-            _ (println (count chars) read-chars)
             new-prev (nth chars (dec read-chars))]
         (recur new-chars
                new-pos
@@ -124,7 +122,7 @@
   (append-char (append-char n sample) "aaa")
   (append-char (append-children (append-char n sample) n) "aaa")
   (parse-next-char sample nil)
-  (parse-text sample n)
+  (parse-text "**foo bar** \n" n)
   (def k #(-> %
               (string/replace "var" "(def")
               (string/replace #"C_\S+" (fn [s] (-> s (subs 2)
