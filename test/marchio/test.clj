66d0c42c11578fb5375e749c064430f9dce327a1
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-10-30 17:08:06 +0200

    Add a thousand failing tests from the spec
    Check that both the AST and the HTML output are correct.

---
+++ test/marchio/test.clj
@@ -0,0 +1,54 @@
+(ns marchio.test
+  (:require
+    [clojure.java.shell :refer [sh]]
+    [clojure.data.xml :as xml]
+    [clojure.walk :refer [postwalk]]
+    [clojure.test :refer [use-fixtures deftest testing is]]
+    [cheshire.core :refer [parse-string generate-string]]
+    [marchio.render :as render]
+    [marchio.ast :as ast :refer [new-node]])
+  (:import (clojure.data.xml.node Element)))
+
+(defn cmark->marchio
+  "Convert from cmark xml to marchio AST."
+  [ast]
+  (->> ast
+       (postwalk
+         (fn [el] ;; if is here so that strings don't get wrapped
+           (if (= (type el) Element)
+             (new-node (keyword (name (:tag el)))
+                       (:attrs el)
+                       (:content el))
+             el)))))
+
+(defn get-cmark-ast
+  "Parse markdown with cmark, return marchio AST."
+  [markdown]
+  (xml/declare-ns "commonmark" "http://commonmark.org/xml/1.0")
+  (-> (sh "cmark" "--to" "xml" :in markdown)
+      (:out)
+      (xml/parse-str)
+      (cmark->marchio)))
+
+(defn load-spec-tests
+  "Read spec tests, output a list of test maps."
+  []
+  (-> (sh "python3" "spec/test/spec_tests.py"
+          "--spec" "spec/spec.txt"
+          "--dump-tests")
+      (:out)
+      (parse-string true)))
+
+(defn spec-fixture [f]
+  (def spec-tests (load-spec-tests))
+  (f))
+
+(use-fixtures :once spec-fixture)
+
+(deftest ast-testing
+  (doseq [{:keys [html markdown section example]} spec-tests]
+    (testing (str "AST: " section ", " example)
+      (is (= (get-cmark-ast markdown)
+             (ast/text->ast markdown))))
+    (testing (str "HTML: " section ", " example)
+      (is (= html (render/md->html markdown))))))
