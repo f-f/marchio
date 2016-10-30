7bbd5d3214c62392a0c49e069fc40526596b7888
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-10-30 17:06:56 +0200

    Add first - non-working - parseEmphasis algorithm

--- src/marchio/inlines.clj
+++ src/marchio/inlines.clj
@@ -1,9 +1,8 @@
 (ns marchio.inlines
   (:require
     [clojure.string :as string]
-    [clojure.core.match :refer [match]]
     [marchio.chars :refer [Asterisk Newline Underscore Singlequote Doublequote]]
-    [marchio.re :as re :refer [match-char]]))
+    [marchio.re :as re :refer [match]]))
 
 ;; -- Phase 2
 
@@ -38,9 +37,9 @@
                                    (= char Doublequote))
                              [1           (rest chars)]
                              [(count run) run-comp])
-        [before-white? after-white?] (mapv #(match-char re/unicode-whitespace %)
+        [before-white? after-white?] (mapv #(match re/unicode-whitespace %)
                                            [before after])
-        [before-punct? after-punct?] (mapv #(match-char re/punctuation %)
+        [before-punct? after-punct?] (mapv #(match re/punctuation %)
                                            [before after])
         left-flanking (and (not after-white?)
                            (not (and after-punct?
@@ -71,16 +70,93 @@
         :opener? opener?
         :closer? closer?}])))
 
-;; TODO: implement
-;; TODO: trasform to \u2019 if singlequote, \u201C if double
+(defn index-of
+  "Returns the index of first el in coll if found, otherwise nil"
+  [coll el]
+  (let [not-neg? (complement neg?)
+        index    (.indexOf coll el)]
+    (when (not-neg? index)
+      index)))
+
+(defn index-of-next
+  "Return the index of the next char in the chars types list"
+  [char chars-types]
+  (when-let [index (-> chars-types (subvec 1) (index-of char))]
+    (inc index)))
+
+(defn delimiter?
+  "Takes a char, returns true if it's a delimiter"
+  [char]
+  (some #{char} [Underscore Asterisk Singlequote Doublequote]))
+
+(defn next-delimiter
+  "Returns the next delimiter index after the current"
+  [current char-types]
+  (let [steps-from-current (->> (subvec char-types (inc current))
+                                (take-while (complement delimiter?))
+                                (count))
+        next (+ 1 current steps-from-current)]
+    (when (< next (count char-types))
+      next)))
+
+(defn normalize-text
+  "Converts candidate delimiters to normal text in case of no match"
+  [node]
+  (update node :children
+    #(for [c %]
+      (let [char (:type c)]
+        (if (delimiter? char)
+          {:type :text
+           :attrs {}
+           :children [(string/join (repeat (:delims c) char))]}
+          c)))))
+
+(defn previous-opener
+  "Returns the previous opener before the closer index, of the same char."
+  [closer closer-char children openers-bottom]
+  (let [children-stripe (reverse (subvec children
+                                         (get openers-bottom closer-char)
+                                         closer))
+        steps-back (->> children-stripe
+                        (take-while #(or (not= closer-char (:type %))
+                                         (not (:opener? %))))
+                        (count))
+        previous (- closer steps-back 1)]
+    previous))
+
 (defn process-emph
   "Process the children of the current node to generate emphasis inlines.
   Stack bottom is the index of the child to wich we can go back."
   [node stack-bottom]
-  node)
-;(let [children (subvec (:children node) stack-bottom)]
-;  (loop [current-position (min-pos (.indexOf (map :type children) Underscore)
-;                                   (.indexOf (map :type children) Asterisk)])
+  ;; TODO: trasform to \u2019 if singlequote, \u201C if double
+  (let [children (subvec (:children node) stack-bottom)
+        children-types (mapv :type children)]
+    (loop [current-node node
+           openers-bottom {Asterisk    stack-bottom
+                           Underscore  stack-bottom
+                           Singlequote stack-bottom
+                           Doublequote stack-bottom}
+           closer (->> [Asterisk Underscore]
+                       (mapv #(index-of-next % children-types))
+                       (remove nil?)
+                       (#(when (not-empty %)
+                          (apply min %))))]
+      (if closer
+        (let [closer-node (nth children closer)
+              char (:type closer-node)
+              opener (previous-opener closer char children openers-bottom)
+              opener-node (nth children opener)
+              odd-match? (and (or (:opener? closer-node)
+                                  (:closer? opener-node))
+                              (zero? (mod (+ (:delims opener-node)
+                                             (:delims closer-node))
+                                          3)))]
+          (if (and (:closer? closer-node) opener)
+            opener-node
+            (recur current-node
+                   openers-bottom
+                   (next-delimiter closer children-types))))
+        (normalize-text current-node)))))
 
 (defn parse-next-char
   "Take a sequence of chars, return a new sequence of the rest of the chars and
