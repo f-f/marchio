6d3b963ac05f352d6f880a7693df4674df7d4eea
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-10-31 22:59:30 +0200

    Add first working implementation of emphasis parsing

--- src/marchio/inlines.clj
+++ src/marchio/inlines.clj
@@ -1,13 +1,16 @@
 (ns marchio.inlines
   (:require
     [clojure.string :as string]
+    [uncomplicate.fluokitten.core :refer [fmap fapply bind return fold foldmap]]
+    [uncomplicate.fluokitten.jvm]
     [marchio.chars :refer [Asterisk Newline Underscore Singlequote Doublequote]]
     [marchio.re :as re :refer [match]]
     [marchio.ast :refer [new-node]]))
 
 ;; -- Phase 2
-
 ;; -- Inline parsers
+
+(def not-neg? (complement neg?))
 
 (defn append-char [node char]
   (update node :content
@@ -66,7 +69,7 @@
                             [left-flanking right-flanking])]
     (when-not (and (empty? chars) (zero? delims))
       [new-chars
-       {:type char
+       {:tag char
         :delims delims
         :opener? opener?
         :closer? closer?}])))
@@ -74,8 +77,7 @@
 (defn index-of
   "Returns the index of first el in coll if found, otherwise nil"
   [coll el]
-  (let [not-neg? (complement neg?)
-        index    (.indexOf coll el)]
+  (let [index    (.indexOf coll el)]
     (when (not-neg? index)
       index)))
 
@@ -91,7 +93,8 @@
   (some #{char} [Underscore Asterisk Singlequote Doublequote]))
 
 (defn next-delimiter
-  "Returns the next delimiter index after the current"
+  "Takes an index and a collection of tags.
+   Returns the next delimiter index after the current."
   [current char-types]
   (let [steps-from-current (->> (subvec char-types (inc current))
                                 (take-while (complement delimiter?))
@@ -100,18 +103,6 @@
     (when (< next (count char-types))
       next)))
 
-(defn normalize-text
-  "Converts candidate delimiters to normal text in case of no match"
-  [node]
-  (update node :children
-    #(for [c %]
-      (let [char (:type c)]
-        (if (delimiter? char)
-          {:type :text
-           :attrs {}
-           :children [(string/join (repeat (:delims c) char))]}
-          c)))))
-
 (defn previous-opener
   "Returns the previous opener before the closer index, of the same char."
   [closer closer-char children openers-bottom]
@@ -119,45 +110,126 @@
                                          (get openers-bottom closer-char)
                                          closer))
         steps-back (->> children-stripe
-                        (take-while #(or (not= closer-char (:type %))
+                        (take-while #(or (not= closer-char (:tag %))
                                          (not (:opener? %))))
                         (count))
-        previous (- closer steps-back 1)]
-    previous))
+        previous (- closer
+                    steps-back
+                    (get openers-bottom closer-char)
+                    1)]
+    (when (not-neg? previous) previous)))
+
+(defn delimiter->text
+  "Converts a delimiter to a text node."
+  [{:keys [tag delims]}]
+  (new-node :text (string/join (repeat delims tag))))
+
+(defn remove-delimiters
+  "Converts candidate delimiters to normal text in case of no match.
+   Multiple arity operates only between start and end - both not included."
+  [children]
+  (map #(if (delimiter? (:tag %))
+         (delimiter->text %)
+         %)
+       children))
+
+(defn compact-text-nodes
+  "Given a list of text nodes, join together the contiguous ones."
+  [children]
+  (reduce (fn [new-vec el]
+            (if (and (= (:tag el) :text)
+                     (= (:tag (last new-vec)) :text))
+              (conj (pop new-vec)
+                    (update-in (last new-vec)
+                               [:content 0]
+                               #(str % (first (:content el)))))
+              (conj new-vec el)))
+          []
+          children))
+
+(defn update-delimiters
+  "Subtracts the given delimiters to the node n. If new count is 0, retuns nil.
+   WARNING: returns a list with one element or nil."
+  [n delims]
+  (let [new-ds (- (:delims n) delims)]
+    (when (pos? new-ds)
+      [(assoc n :delims new-ds)])))
+
+(defn normalize-node
+  "Finalize the node after having processed emphasis"
+  [node]
+  (update node :content #(-> % remove-delimiters compact-text-nodes)))
 
 (defn process-emph
   "Process the children of the current node to generate emphasis inlines.
   Stack bottom is the index of the child to wich we can go back."
   [node stack-bottom]
   ;; TODO: trasform to \u2019 if singlequote, \u201C if double
-  (let [children (subvec (:children node) stack-bottom)
-        children-types (mapv :type children)]
+  (let [init-children (subvec (:content node) stack-bottom)
+        init-children-types (mapv :tag init-children)]
     (loop [current-node node
            openers-bottom {Asterisk    stack-bottom
                            Underscore  stack-bottom
                            Singlequote stack-bottom
                            Doublequote stack-bottom}
-           closer (->> [Asterisk Underscore]
-                       (mapv #(index-of-next % children-types))
-                       (remove nil?)
-                       (#(when (not-empty %)
-                          (apply min %))))]
-      (if closer
-        (let [closer-node (nth children closer)
-              char (:type closer-node)
-              opener (previous-opener closer char children openers-bottom)
-              opener-node (nth children opener)
-              odd-match? (and (or (:opener? closer-node)
-                                  (:closer? opener-node))
-                              (zero? (mod (+ (:delims opener-node)
-                                             (:delims closer-node))
-                                          3)))]
-          (if (and (:closer? closer-node) opener)
-            opener-node
-            (recur current-node
-                   openers-bottom
-                   (next-delimiter closer children-types))))
-        (normalize-text current-node)))))
+           closer (as-> [Asterisk Underscore] cs
+                        (mapv #(index-of-next % init-children-types) cs)
+                        (remove nil? cs)
+                        (when (not-empty cs)
+                          (apply min cs)))]
+      (let [children (:content current-node)
+            children-types (mapv :tag children)]
+        (if-not closer
+          (normalize-node current-node)
+          (let [closer-node (nth children closer)
+                char (:tag closer-node)
+                opener (previous-opener closer char children openers-bottom)
+                opener-node (when opener
+                              (nth children opener))
+                opener-delims (:delims opener-node)
+                closer-delims (:delims closer-node)]
+            (if (and (:closer? closer-node) opener)
+              ;; Good, we found a matching opener-closer pair! Things to do:
+              ;; 1. Figure out if it's emph or strong emph
+              ;; 2. Put all the nodes between the opener and closer inside
+              ;;    the new
+              ;; 3. Convert the delims inside to text, compact the text nodes
+              ;; 4. Insert the new node *after* the opener
+              ;; 5. Subtract the # of delimiters from opener and closer
+              ;; 6. If a delimiter becomes empty, remove it.
+              (let [delims (min 2 opener-delims closer-delims)
+                    strong? (> delims 1) ;; 1.
+                    emph-node (new-node (if strong? :strong :emph)
+                                        {}
+                                        (-> children
+                                            (subvec (inc opener) closer) ; 2.
+                                            (remove-delimiters) ; 3a.
+                                            (compact-text-nodes))) ; 3b.
+                    [new-op new-cl] (mapv #(update-delimiters % delims) ; 5, 6.
+                                          [opener-node closer-node])
+                    new-children (fold [(subvec children 0 opener)
+                                        new-op
+                                        [emph-node] ; 4.
+                                        new-cl
+                                        (subvec children (inc closer))])
+                    right-number (- (count children)
+                                    (count new-children))
+                    new-children-types (mapv :tag new-children)]
+                (recur (assoc current-node :content new-children)
+                       openers-bottom
+                       (next-delimiter (- closer right-number)
+                                       new-children-types)))
+              ;; No opener around here. We proceed to:
+              ;; 1. Advance the stack bottom for the current char to current
+              ;; 2. Convert the current node to text if also not an opener
+              ;; 3. Advance the current element
+              (recur (if-not (:opener? closer-node) ; 2.
+                       (update-in current-node
+                                  [:content closer]
+                                  delimiter->text)
+                       current-node)
+                     (assoc openers-bottom char (dec closer)) ; 1.
+                     (next-delimiter closer children-types))))))))) ; 3.
 
 (defn parse-next-char
   "Take a sequence of chars, return a new sequence of the rest of the chars and
@@ -195,14 +267,11 @@
 
 (comment
   (def sample "*foo bar*\n")
+  (def sample2 "**foo bar** \n")
   (def n (marchio.ast/new-node :paragraph))
   (append-char (append-char n sample) "aaa")
   (append-char (append-children (append-char n sample) n) "aaa")
   (parse-next-char sample nil)
-  (parse-text "**foo bar** \n" n)
-  (def k #(-> %
-              (string/replace "var" "(def")
-              (string/replace #"C_\S+" (fn [s] (-> s (subs 2)
-                                                   string/capitalize)))
-              (string/replace "= " "(char ")
-              (string/replace ";" "))"))))
+  (marchio.test/get-cmark-ast sample)
+  (marchio.test/get-cmark-ast sample2)
+  (parse-text sample2 n))
