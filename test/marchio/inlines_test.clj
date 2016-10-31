4c2b2e524d4d6cff4e6e16bf7b43542a3477ec13
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-10-31 22:59:48 +0200

    Add tests for emphasis parsing

--- test/marchio/inlines_test.clj
+++ test/marchio/inlines_test.clj
@@ -1,1 +1,22 @@
-(ns marchio.inlines-test)
+(ns marchio.inlines-test
+  (:require
+    [clojure.test :refer [use-fixtures deftest testing is]]
+    [marchio.inlines :as inlines]
+    [marchio.ast :refer [new-node]])
+  (:import (marchio.ast Node)))
+
+(def n (new-node :paragraph))
+
+(deftest parse-test
+  (testing "Emphasis parsing 1"
+    (is (instance? Node (inlines/parse-text "*foo bar*" n))))
+  (testing "Emphasis parsing 2"
+    (is (instance? Node (inlines/parse-text "*foo bar" n))))
+  (testing "Emphasis parsing 3"
+    (is (instance? Node (inlines/parse-text "**foo bar**" n))))
+  (testing "Emphasis parsing 4"
+    (is (instance? Node (inlines/parse-text "**foo** bar*" n))))
+  (testing "Emphasis parsing 5"
+    (is (instance? Node (inlines/parse-text "***foo** bar*" n))))
+  (testing "Emphasis parsing 6"
+    (is (instance? Node (inlines/parse-text "foo** bar*" n)))))
