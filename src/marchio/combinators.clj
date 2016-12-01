b71d65a931d2b7a6dee4b98de006701fe458d2c8
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-12-01 12:56:57 +0200

    Add defparser macro

--- src/marchio/combinators.clj
+++ src/marchio/combinators.clj
@@ -1,6 +1,13 @@
 (ns marchio.combinators
   (:require [blancas.kern.core :as k]))
 
+;; TODO: spec it
+(defmacro defparser
+  "Boilerplate for a backtracking parser"
+  [name bindings return-body]
+  `(def ~name
+     (k/<:> (k/bind ~bindings (k/return ~return-body)))))
+
 (def any
   "Parser that accepts any character as input, returns string."
   (k/<+> (k/many k/any-char)))
