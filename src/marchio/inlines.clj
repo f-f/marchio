2442564a5c2cb81b3ee2d6c8876cef837b7a4c7a
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-12-02 17:12:54 +0200

    Add inline parsers: emphasis delimiters generation

--- src/marchio/inlines.clj
+++ src/marchio/inlines.clj
@@ -22,60 +22,6 @@
               (assoc children (dec (count children))
                               (str (last children) char))))))
 
-(defn append-children [node new]
-  (update node :content conj new))
-
-(defn parse-emphasis
-  "Search for delimiter runs (asterisk, underscore) or quotes, and return a
-   vector with the rest of the string and a new Delimiter entity, with info
-   about the length of the run, and if they can open or close sequences."
-  [string prev-char]
-  (let [chars  (seq string)
-        before (or (if (char? prev-char)
-                     prev-char
-                     (first (seq prev-char)))
-                   c/Newline)
-        char   (first chars)
-        after  (or (second chars) c/Newline)
-        [run run-comp] (mapv #(% (partial = char) chars)
-                             [take-while drop-while])
-        [delims new-chars] (if (or (= char c/Singlequote)
-                                   (= char c/Doublequote))
-                             [1           (rest chars)]
-                             [(count run) run-comp])
-        [before-white? after-white?] (mapv #(match re/unicode-whitespace %)
-                                           [before after])
-        [before-punct? after-punct?] (mapv #(match re/punctuation %)
-                                           [before after])
-        left-flanking (and (not after-white?)
-                           (not (and after-punct?
-                                     (not before-white?)
-                                     (not before-punct?))))
-        right-flanking (and (not before-white?)
-                            (not (and before-punct?
-                                      (not after-white?)
-                                      (not after-punct?))))
-        underscore-flags [(and left-flanking
-                               (or (not right-flanking)
-                                   before-punct?))
-                          (and right-flanking
-                               (or (not left-flanking)
-                                   after-punct?))]
-        quotes-flags [(and left-flanking
-                           (not right-flanking))
-                      right-flanking]
-        [opener? closer?] (condp = char
-                            c/Underscore  underscore-flags
-                            c/Singlequote quotes-flags
-                            c/Doublequote quotes-flags
-                            [left-flanking right-flanking])]
-    (when-not (and (empty? chars) (zero? delims))
-      [new-chars
-       {:tag char
-        :delims delims
-        :opener? opener?
-        :closer? closer?}])))
-
 (defn index-of
   "Returns the index of first el in coll if found, otherwise nil"
   [coll el]
@@ -233,32 +179,7 @@
                      (assoc openers-bottom char (dec closer)) ; 1.
                      (next-delimiter closer children-types))))))))) ; 3.
 
-(defn parse-next-char
-  "Take a sequence of chars, return a new sequence of the rest of the chars and
-   a new node."
-  [chars prev-char]
-  (let [c (first chars)]
-    (condp = c
-      c/Asterisk   (parse-emphasis chars prev-char)
-      c/Underscore (parse-emphasis chars prev-char)
-      [(rest chars)
-       (new-node :text (str c))])))
 
-(defn parse-text [line block]
-  (loop [chars (seq line)
-         pos   0
-         prev  nil
-         node  block]
-    (if (empty? chars)
-      (process-emph node 0)
-      (let [[new-chars new-node] (parse-next-char chars prev)
-            read-chars (- (count chars) (count new-chars))
-            new-pos (+ pos read-chars)
-            new-prev (nth chars (dec read-chars))]
-        (recur new-chars
-               new-pos
-               new-prev
-               (append-children node new-node))))))
 
 ;; -- Parsers ------------------------------------------------------------------
 
@@ -298,9 +219,60 @@
   [ticks (k/many1 (k/sym* c/Backtick))]
   (new-node :text (apply str ticks)))
 
+;; Delimiters runs (asterisk, underscore) or quotes.
+;; Info returned: run length, and if they can open and/or close sequences.
+(defparser Emph
+  [s     k/get-state
+   p     k/get-position
+   c     (k/<|> (k/sym* c/Singlequote)
+                (k/sym* c/Doublequote)
+                (k/many1 (k/sym* c/Asterisk))
+                (k/many1 (k/sym* c/Underscore)))
+   after (k/look-ahead k/any-char)]
+  (let [[num char] (if (vector? c)
+                     [(count c) (first c)]
+                     [1         c])
+        bef-white? (and (= (:line p)      (:line (:white s)))
+                        (= (dec (:col p)) (:col  (:white s))))
+        bef-punct? (and (= (:line p)      (:line (:punct s)))
+                        (= (dec (:col p)) (:col  (:punct s))))
+        aft-white? (match re/unicode-whitespace after)
+        aft-punct? (match re/punctuation after)
+        left-flanking (and (not aft-white?)
+                           (not (and aft-punct?
+                                     (not bef-white?)
+                                     (not bef-punct?))))
+        right-flanking (and (not bef-white?)
+                            (not (and bef-punct?
+                                      (not aft-white?)
+                                      (not aft-punct?))))
+        underscore-flags [(and left-flanking
+                               (or (not right-flanking)
+                                   bef-punct?))
+                          (and right-flanking
+                               (or (not left-flanking)
+                                   aft-punct?))]
+        quotes-flags [(and left-flanking
+                           (not right-flanking))
+                      right-flanking]
+        [opener? closer?] (condp = char
+                            c/Underscore  underscore-flags
+                            c/Singlequote quotes-flags
+                            c/Doublequote quotes-flags
+                            [left-flanking right-flanking])]
+    {:tag char
+     :delims num
+     :opener? opener?
+     :closer? closer?}))
+
 ;; Fallback, just text
 (defparser Text
-  [t k/any-char]
+  [p k/get-position
+   t k/any-char
+   _ (cond
+       (re/match re/unicode-whitespace t) (k/modify-state #(assoc % :white p))
+       (re/match re/punctuation t)        (k/modify-state #(assoc % :punct p))
+       :else k/get-state)]
   (new-node :text (str t)))
 
 (def Inlines
@@ -310,7 +282,7 @@
          Backslash
          InlineCode
          Backticks
-         ;EmphDelimiter
+         Emph
          ;Brackets
          ;Bang
          ;LessTHan
@@ -352,4 +324,5 @@
       (ast/append-child (new-node :paragraph "aaa\nbbb  \nfoo"))
       (ast/append-child (new-node :paragraph "\\["))
       (ast/append-child (new-node :paragraph "``aa`b`"))
+      (ast/append-child (new-node :paragraph sample2))
       (parse)))
