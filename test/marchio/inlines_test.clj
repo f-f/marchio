9bf32c0b71796e070b0ad84c29bdbf226ba7aa72
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-12-02 17:12:29 +0200

    Fix test format with the new inline parsing API

--- test/marchio/inlines_test.clj
+++ test/marchio/inlines_test.clj
@@ -5,18 +5,16 @@
     [marchio.ast :refer [new-node]])
   (:import (marchio.ast Node)))
 
-(def n (new-node :paragraph))
-
 (deftest parse-test
   (testing "Emphasis parsing 1"
-    (is (instance? Node (inlines/parse-text "*foo bar*" n))))
+    (is (vector? (inlines/parse-line ["*foo bar*"]))))
   (testing "Emphasis parsing 2"
-    (is (instance? Node (inlines/parse-text "*foo bar" n))))
+    (is (vector? (inlines/parse-line ["*foo bar"]))))
   (testing "Emphasis parsing 3"
-    (is (instance? Node (inlines/parse-text "**foo bar**" n))))
+    (is (vector? (inlines/parse-line ["**foo bar**"]))))
   (testing "Emphasis parsing 4"
-    (is (instance? Node (inlines/parse-text "**foo** bar*" n))))
+    (is (vector? (inlines/parse-line ["**foo** bar*"]))))
   (testing "Emphasis parsing 5"
-    (is (instance? Node (inlines/parse-text "***foo** bar*" n))))
+    (is (vector? (inlines/parse-line ["***foo** bar*"]))))
   (testing "Emphasis parsing 6"
-    (is (instance? Node (inlines/parse-text "foo** bar*" n)))))
+    (is (vector? (inlines/parse-line ["foo** bar*"])))))
