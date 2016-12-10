0519a7300cd36d67ec7a1e856db36685fb61bf93
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2016-12-10 13:37:14 +0200

    Fix test looks: much better looks and diffs with ultra plugin

--- project.clj
+++ project.clj
@@ -25,6 +25,7 @@
 
   :plugins [[lein-cljsbuild "1.1.4"]
             [lein-codox "0.10.0"]
+            [venantius/ultra "0.5.0"]
             [lein-bin "0.3.5"]]
 
   :aliases {"build-once" ["cljsbuild" "once"]
